64815d2026c5354a790dc08046ec88f3ef0e8da3
Bo Zhang <zhangbo0037@gmail.com>
2024-12-19 19:04:58 +0800

    update

--- run_ltxvideo.py
+++ run_ltxvideo.py
@@ -13,18 +13,21 @@ ltx.set_prompt(
     negative_prompt=PromptManager("prompts.json").get("negative-video")
 )
 
+# refer settings can make a video on Mac: 
+# [768, 512], fps=24, num_frames = 121, around 5 seconds video
+# [1280, 720], fps=24, num_frames = 49, around 2 seconds video
 ltx.set_output_layout(
-    width= 1280, 
-    height=720, 
-    frame_rate=8, 
-    num_frames=25, 
+    width=768, 
+    height=512, 
+    frame_rate=24, 
+    num_frames=121, 
     num_inference_steps=50
 )
 
 ltx.set_stg(
     stg_mode="stg-a",   # Choose between 'stg-a' or 'stg-r'
-    stg_scale=1.25,      # Recommended values are ≤2.0, (stg_scale = 0.0 means do not using stg)
-    stg_block_idx=[19],   # Specify the block index for applying STG
+    stg_scale=0,      # Recommended values are ≤2.0, (stg_scale = 0.0 means do not using stg)
+    stg_block_idx=[],   # Specify the block index for applying STG
     do_rescaling=True  # Set to True to enable rescaling
 )
 
