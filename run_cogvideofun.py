1de717edea8ce7bba44038cbc6f04c5e1c5d0749
Bo Zhang <zhangbo0037@gmail.com>
2024-12-19 18:18:19 +0800

    update

--- run_cogvideofun.py
+++ run_cogvideofun.py
@@ -4,27 +4,27 @@ from src.CogVideoFunManager import CogVideoFunManager
 from src.PromptManager import PromptManager
 
 # Init
-should_download = True # should download at first time only
+should_download = False # should download at first time only
 device = set_device()
 cogVideoFun = CogVideoFunManager(device, torch.bfloat16)
 
 # rewrite prompt to video mode
 prompt_manager = PromptManager("prompts.json")
 original_prompt = prompt_manager.get("suv")
-rewrited_prompt = prompt_manager.rewriter.rewrite_video_prompt(original_prompt)
+#rewrited_prompt = prompt_manager.rewriter.rewrite_video_prompt(original_prompt)
 
-# settings
-# can make a video with on Mac: 
-# fps=8, num_frames = 24, around 3 seconds video
-# fps=6, num_frames = 24, around 4 seconds video
-cogVideoFun.set_prompt(prompt=rewrited_prompt)
+# refer settings can make a video on Mac: 
+# [768, 432], fps=8, num_frames = 24, around 3 seconds video
+# [640, ???], fps=8, num_frames = 33, around 4 seconds video
+# [480, 272], fps=8, num_frames = 41, around 5 seconds video
+cogVideoFun.set_prompt(prompt=original_prompt)
 cogVideoFun.set_validation_images(validation_image_start="assets/imgs/suv.png")
 cogVideoFun.set_output_layout(
     output_path="output_cogvideox_fun", 
-    width= 768, 
-    height=432,
+    width= 480, 
+    height=272,
     fps=8,
-    num_frames=24,
+    num_frames=41,
     num_inference_steps=50
 )
 
